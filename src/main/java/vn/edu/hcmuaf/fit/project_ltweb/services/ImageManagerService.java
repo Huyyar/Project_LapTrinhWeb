fc15fc25d47a2dc9910a8dbdf1ded76afe09a126
phnd-2706 <23130057@st.hcmuaf.edu.vn>
2026-01-26 17:38:36 +0700

    sửa lỗi mở modal trang quản lí sản phẩm admim, sửa trang quản lí hình ảnh admin

--- src/main/java/vn/edu/hcmuaf/fit/project_ltweb/services/ImageManagerService.java
+++ src/main/java/vn/edu/hcmuaf/fit/project_ltweb/services/ImageManagerService.java
@@ -1,82 +1,59 @@
 package vn.edu.hcmuaf.fit.project_ltweb.services;
 
+import jakarta.servlet.http.Part;
 import vn.edu.hcmuaf.fit.project_ltweb.model.ImageFile;
 import vn.edu.hcmuaf.fit.project_ltweb.utils.FileUtil;
-
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ImageManagerService {
-    private String uploadPath;
+    private final String srcPath;
+    private final String deployPath;
 
     public ImageManagerService(String root) {
-        this.uploadPath = FileUtil.getUploadPath(root);
-    }
-    public String getUploadPath() {
-        return uploadPath;
+        this.srcPath = FileUtil.getSrcPath(root);
+        this.deployPath = FileUtil.getDeployPath(root);
     }
 
-    public int totalImages() {
-        return FileUtil.totalFiles(uploadPath);
+    public void uploadImage(Part part, String name) throws IOException {
+        FileUtil.saveDualFile(part, name, srcPath, deployPath);
     }
 
-    public List<ImageFile> getPagedImages(int offset, int pageSize) {
-        List<ImageFile> images = new ArrayList<>();
-        List<File> files = FileUtil.getPagedFiles(uploadPath, offset, pageSize);
-        if (files == null) {
-            return images;
-        }
-        for(File file : files){
-            ImageFile image = new ImageFile();
-            image.setName(file.getName());
-            image.setUrl("/assets/images/" + file.getName());
-            image.setSize(file.length());
-
-            images.add(image);
-        }
-
-        return images;
-    }
     public List<ImageFile> getPagedSearchImages(int offset, int pageSize, String search) {
         List<ImageFile> images = new ArrayList<>();
-        List<File> files = FileUtil.getPagedSearchFiles(uploadPath, offset, pageSize, search);
-        if (files == null) {
-            return images;
+        List<File> files = FileUtil.getPagedSearchFiles(deployPath, offset, pageSize, search);
+        for (File f : files) {
+            images.add(mapToFileModel(f));
         }
-        for(File file : files){
-            ImageFile image = new ImageFile();
-            image.setName(file.getName());
-            image.setUrl("/assets/images/" + file.getName());
-            image.setSize(file.length());
+        return images;
+    }
 
-            images.add(image);
-        }
-        return  images;
-    }
-    public int totalSearchImages(String search){
-        return FileUtil.totalSearchFiles(uploadPath, search);
-    }
     public ImageFile getImage(String name) {
-        ImageFile image = new ImageFile();
-        File f = FileUtil.getFile(uploadPath, name);
-        if(f.exists()){
-            image.setName(f.getName());
-            image.setUrl("/assets/images/" + f.getName());
-            image.setSize(f.length());
-            return image;
-        }
-        return null;
-    }
-    public int totalImagesSearch(String search){
-        return FileUtil.totalSearchFiles(uploadPath, search);
+        File f = new File(deployPath, name);
+        return f.exists() ? mapToFileModel(f) : null;
     }
 
-    public void deleteImage(String name){
-        FileUtil.deleteFile(uploadPath +"/"+ name);
-    }
-    public void renameImage(String name, String newName){
-        FileUtil.renameFile(uploadPath +"/"+ name, newName);
+    public void deleteImage(String name) {
+        FileUtil.deleteFile(srcPath + File.separator + name);
+        FileUtil.deleteFile(deployPath + File.separator + name);
     }
 
+    public void renameImage(String name, String newName) {
+        FileUtil.renameFile(srcPath + File.separator + name, newName);
+        FileUtil.renameFile(deployPath + File.separator + name, newName);
+    }
+
+    public int totalSearchImages(String search) {
+        return FileUtil.totalSearchFiles(deployPath, search);
+    }
+
+    private ImageFile mapToFileModel(File f) {
+        ImageFile img = new ImageFile();
+        img.setName(f.getName());
+        img.setUrl("/assets/images/" + f.getName());
+        img.setSize(f.length());
+        return img;
+    }
 }
